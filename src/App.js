a79e86b3c549af3fc7d9ba16eba119f7c25cb779
Josh2580 <joshuaugwuede@gmail.com>
2022-08-25 13:45:02 +0100

    1. Initial Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from "react";
+import {useSelector, useDispatch} from 'react-redux';
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Hello Ignite</h1>      
+    </div>
+  );
+}
+
+export default App;
